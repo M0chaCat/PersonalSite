3f27edf61c3ff98d3817dd0edc4dd3d6c5ea7b57
Arcane <74067110+M0chaCat@users.noreply.github.com>
2024-08-26 00:55:44 -0400

    signed

--- Plugins/kbsounds.nkplugin.js
+++ Plugins/kbsounds.nkplugin.js
@@ -169,4 +169,4 @@ class KeyboardSounds {
     this.handleVolumeChange(userPreferences.volume);
   }
 }
-exports.default = KeyboardSounds;
+exports.default = KeyboardSounds;/*NEKOCORD_TEAM_{"signed_by":"M0cha","signed_at":1724648107668}*//*SIGNATURE_TyXR6AZpPx7iizNQNQI7K5shohzEGu6Tu6a38BHPTrD7n5Nxl4345iT5ZBzOiOThXmdvR40A97RNYiW/6W+6WQ==*/
