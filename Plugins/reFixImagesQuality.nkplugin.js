e5701a50612e9f4a2704f42ee8b6f1bc59d0530d
M0chaCat <evan2838383@gmail.com>
2024-12-01 22:30:57 -0500

    Re!FixImagesQuality

---
+++ Plugins/reFixImagesQuality.nkplugin.js
@@ -0,0 +1,40 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+class refiximagesquality {
+  constructor(userPreferences) {
+    this.userPreferences = userPreferences;
+  }
+  info = {
+    name: "Re!FixImagesQuality",
+    id: "cat.kitties.arcane.refiximagesquality",
+    authors: [{
+      name: "Mocha",
+      id: "808802000224518264"
+    }],
+    description: "Prevents images from being loaded as webp, which can cause quality loss",
+    version: "0.1.0",
+    patches: [
+      {
+          find: "getFormatQuality(){",
+          replacement: {
+              match: /(?<=null;return )\i\.\i&&\(\i\|\|!\i\.isAnimated.+?:(?=\i&&\(\i="png"\))/,
+              replace: ""
+          }
+      }
+  ],
+
+    preferences: [],
+  };
+
+  onRegister(userPreferences) {
+    this.userPreferences = userPreferences;
+  }
+
+}
+
+exports.default = refiximagesquality;
